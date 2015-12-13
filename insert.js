7c9d7e0b526df498519b898713fe18e6594faf9b
Dino <dinomandic@hotmail.com>
2015-12-13 11:41:01 +0100

    Refactor code

--- insert.js
+++ insert.js
@@ -11,8 +11,8 @@ var mongo = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/learnyoumongo';
 
 var doc = {
-    'firstName' = process.argv[2],
-    'lastName' = process.argv[3]
+    firstName = process.argv[2],
+    lastName = process.argv[3]
 };
 
 mongo.connect(url, function(err, db) {
@@ -22,10 +22,8 @@ mongo.connect(url, function(err, db) {
 
     docs.insert(doc, function(err, data) {
         if (err) throw err;
-        console.log(doc);
+        console.log(JSON.stringify(doc));
         db.close();
     });
 
 });
-
-va dino = "dsti";
